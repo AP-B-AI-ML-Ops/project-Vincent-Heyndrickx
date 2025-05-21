95d9d9086ded3cabc61ea073a418165ba1ffad95
Vincent <s149949@ap.be>
2025-05-21 11:23:13 +0200

    remove unused file

--- data_batch/load_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_stock_data(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path, parse_dates=['Date'])
-    df.sort_values('Date', inplace=True)
-    df.fillna(method='ffill', inplace=True)
-    df = df[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
-    return df
