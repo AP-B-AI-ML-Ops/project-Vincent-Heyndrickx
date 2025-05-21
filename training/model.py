dc8199b45d99c233cc4980ac5080e330edac3fa5
Vincent <s149949@ap.be>
2025-05-21 11:59:08 +0200

    train model and add mlflow

--- training/model.py
+++ training/model.py
@@ -1,38 +1,46 @@
 import pandas as pd
+import pickle
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import joblib
 import mlflow
-from evidently.report import Report
-from evidently.metric_preset import DataDriftPreset, DataQualityPreset
+import os
 
-def train_model(df: pd.DataFrame):
-    df['Return'] = df['Close'].pct_change()
-    df['Target'] = df['Return'].shift(-1)
-    df.dropna(inplace=True)
+@flow(log_prints=True)
+def train_model(file_path="data/SPX.csv", alpha=1.0):
+    os.makedirs("ML_models", exist_ok=True)
+    df = pd.read_csv(file_path)
+
+    df['Target'] = df['Close'].pct_change().shift(-1)
+    df = df.dropna()
 
     X = df[['Open', 'High', 'Low', 'Close', 'Volume']]
     y = df['Target']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = Ridge()
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    print(f"Test MSE: {mse:.5f}")
-
-    joblib.dump(model, 'model.joblib')
+    mlflow.set_experiment("sp500_regression")
 
     with mlflow.start_run():
+
+        model = Ridge(alpha=alpha)
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        print(f"Test MSE: {mse:.5f}")
+
+
+        mlflow.log_param("alpha", alpha)
         mlflow.log_metric("mse", mse)
         mlflow.sklearn.log_model(model, "model")
 
-    # Evidently report
-    report = Report(metrics=[DataDriftPreset(), DataQualityPreset()])
-    report.run(reference_data=X_train, current_data=X_test)
-    report.save_html("reports/evidently_report.html")
+        with open("ML_models/ridge_model.pkl", "wb") as f:
+            pickle.dump(model, f)
 
-    return model
+        print(f"✅ Model getraind (alpha={alpha})")
+        print(f"Train MSE: {mse_train:.6f}")
+        print(f"Test  MSE: {mse_test:.6f}")
+
+if __name__ == "__main__":
+    train_model()
